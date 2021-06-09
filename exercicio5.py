827645ba0d3b869678b13f5f458e5912b2d40dc6
cinara-neis <cinara.neis@gmail.com>
2021-06-08 21:47:23 -0300

    Create exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,16 @@
+#x = int(input('Digite um valor inteiro: '))
+#y = int(input('Digite um segundo valo inteiro: '))
+
+#if(x > y):
+    #print('O primeiro valor é maior que o segundo!')
+
+
+
+x = int(input('Digite um valor inteiro: '))
+
+if(x % 2 == 0):
+    print('O numero é par!')
+
+else:
+    print('O numero é ímpar!')
+
